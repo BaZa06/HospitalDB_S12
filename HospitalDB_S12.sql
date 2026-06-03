fc9966a10377ae879a7740beebef89b6557a17a0
BaZa06 <bazeledon@uamv.edu.ni>
2026-06-03 13:53:12 -0600

    Subiendo la creación y consulta de las base de datos existentes

---
+++ HospitalDB_S12.sql
@@ -0,0 +1,28 @@
+--Si existe se borra y si no se crea
+USE MASTER;
+GO
+
+IF DB_ID('HospitalDB') IS NOT NULL
+BEGIN
+	ALTER DATABASE HospitalDB
+	SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+	DROP DATABASE IF EXISTS HospitalDB;
+END
+GO
+
+CREATE DATABASE HospitalDB;
+GO
+
+USE HospitalDB;
+GO
+
+--Consulta para enseñar todas las bases de datos
+SELECT name
+FROM sys.databases;
+GO
+
+--Seleccionar base de datos
+USE HospitalDB;
+GO
+
